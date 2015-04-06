8522e4b76d0cf113b0260258a8570f393aa15648
John Yocum <jtyocum@uw.edu>
2015-04-06 10:58:49 -0700

    user creation works, but it is buggy

--- uwauth/src/Authentication/Provider/UwAuth.php
+++ uwauth/src/Authentication/Provider/UwAuth.php
@@ -9,30 +9,15 @@ namespace Drupal\uwauth\Authentication\Provider;
 
 use Drupal\Component\Utility\String;
 use Drupal\Core\Authentication\AuthenticationProviderInterface;
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityManagerInterface;
-use Drupal\user\UserAuthInterface;
 use Drupal\Core\Session\SessionConfigurationInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Drupal\user\Entity\User;
 
 /**
  * Shibboleth and UW Groups authentication provider.
  */
 class UwAuth implements AuthenticationProviderInterface {
-
-  /**
-   * The config factory.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  protected $configFactory;
-
-  /**
-   * The user auth service.
-   *
-   * @var \Drupal\user\UserAuthInterface
-   */
-  protected $userAuth;
 
   /**
    * The entity manager.
@@ -51,18 +36,12 @@ class UwAuth implements AuthenticationProviderInterface {
   /**
    * Constructs a UW authentication provider object.
    *
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The config factory.
-   * @param \Drupal\user\UserAuthInterface $user_auth
-   *   The user authentication service.
    * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
    *   The entity manager service.
    * @param \Drupal\Core\Session\SessionConfigurationInterface $session_configuration
    *   The session configuration.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, UserAuthInterface $user_auth, EntityManagerInterface $entity_manager, SessionConfigurationInterface $session_configuration) {
-    $this->configFactory = $config_factory;
-    $this->userAuth = $user_auth;
+  public function __construct(EntityManagerInterface $entity_manager, SessionConfigurationInterface $session_configuration) {
     $this->entityManager = $entity_manager;
     $this->sessionConfiguration = $session_configuration;
   }
@@ -92,6 +71,18 @@ class UwAuth implements AuthenticationProviderInterface {
     $account = reset($accounts);
     if ($account) {
       // Immediately refresh after generating session. This will let Drupal's cookie provider take over auth.
+      user_login_finalize($account);
+      header("Refresh: 0");
+    } else {
+      // User entity doesn't exist, so create it, and immediately login
+      $user = User::create(array(
+        'name' => $username,
+      ));
+      $user->save();
+      $accounts = $this->entityManager->getStorage('user')->loadByProperties(array('name' => $username));
+      $account = reset($accounts);
+      $account->activate();
+      $account->save();
       user_login_finalize($account);
       header("Refresh: 0");
     }
