336ad0d97cfc172fc33619d7b271f13c1ab09495
John Yocum <jtyocum@uw.edu>
2015-04-08 08:06:58 -0700

    Role assignment works. Needs error handling

--- uwauth/src/Authentication/Provider/UwAuth.php
+++ uwauth/src/Authentication/Provider/UwAuth.php
@@ -70,6 +70,7 @@ class UwAuth implements AuthenticationProviderInterface {
     $account = reset($this->entityManager->getStorage('user')->loadByProperties(array('name' => $username)));
     if ($account) {
       // Immediately refresh after generating session. This will let Drupal's cookie provider take over auth.
+      $this->sync_roles($account,$username);
       user_login_finalize($account);
       header("Refresh: 0");
     } else {
@@ -87,4 +88,80 @@ class UwAuth implements AuthenticationProviderInterface {
     return [];
   }
 
+  /**
+   * Synchronize roles with UW Groups
+   *
+   * @param $account
+   *   A user object.
+   * @param $username
+   *   An authenticated username.
+   */
+  private function sync_roles($account, $username) {
+    // $account->addRole('test_editor');
+    // $account->removeRole('test_editor');
+    // $account->save();
+    // user_roles(TRUE);
+
+    $roles_existing = user_role_names(TRUE);
+    $roles_assigned = $account->getRoles(TRUE);
+    $uwgws_groups = $this->fetch_uwgroups($username);
+
+    // Remove from roles they are no longer assigned to
+    foreach($roles_assigned as $rid_assigned => $role_assigned) {
+      if (!in_array($role_assigned, $uwgws_groups)) {
+        $account->removeRole($role_assigned);
+      }
+    }
+
+    // Add to newly assigned roles
+    foreach ($uwgws_groups as $uwgroup) {
+      if (in_array($uwgroup, $roles_existing)) {
+        $account->addRole($uwgroup);
+      }
+    }
+
+    $account->save();    
+  }
+
+  /**
+   * Fetch group membership from UW Groups
+   *
+   * @param $username
+   *   An authenticated username.
+   */
+  private function fetch_uwgroups($username) {
+    // UW CA cert base path
+    $uwca_path = '/etc/ssl/';
+
+    // UW GWS URL
+    $uwgws_url = 'https://iam-ws.u.washington.edu/group_sws/v1/search?member=' . $username . '&type=effective&scope=all';
+
+    // Query UW GWS for group membership
+    $uwgws = curl_init();
+    curl_setopt_array($uwgws, array(
+                                CURLOPT_RETURNTRANSFER => TRUE,
+                                CURLOPT_HEADER         => FALSE,
+                                CURLOPT_BINARYTRANSFER => FALSE,
+                                CURLOPT_FOLLOWLOCATION => TRUE,
+                                CURLOPT_SSL_VERIFYHOST => TRUE,
+                                CURLOPT_SSL_VERIFYPEER => TRUE,
+                                CURLOPT_SSLCERT        => $uwca_path.'ehrt_uwca_cert.pem',
+                                CURLOPT_SSLKEY         => $uwca_path.'ehrt_uwca_key.pem',
+                                CURLOPT_CAINFO         => $uwca_path.'ehrt_uwca_ca.pem',
+                                CURLOPT_VERBOSE        => FALSE,
+                                CURLOPT_URL            => $uwgws_url,
+                                ));
+    $uwgws_response = curl_exec($uwgws);
+    curl_close($uwgws);
+
+    // Extract groups from XML feed
+    $uwgws_feed = simplexml_load_string(str_replace('xmlns=', 'ns=', $uwgws_response));
+    $uwgws_entries = $uwgws_feed->xpath("//a[@class='name']");
+    $uwgws_groups = array();
+    while (list( , $node) = each($uwgws_entries)) {
+      $uwgws_groups[] = (string)$node[0];
+    }
+
+    return $uwgws_groups;
+  }
 }
